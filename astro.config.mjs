6d7320e0504a4b576e6923d878507138d12d3bc2
yashjawale <yash25.j@gmail.com>
2025-09-13 17:29:01 +0530

    Update base path in astro.config and modify RSS feed title in BaseHead component

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yashjawale.github.io',
+	base: '/saral-theme',
 	integrations: [
 		mdx(),
 		sitemap(),
